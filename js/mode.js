ec378fb63c8da6ac9f142eb05214a721a8fa56bf
karimaltohamy <karimbadr2003@gmail.com>
2023-09-18 12:22:49 +0200

    commit

--- js/mode.js
+++ js/mode.js
@@ -1,15 +1,27 @@
 const btnMode = document.querySelector(".mode");
+const light = document.querySelector(".light");
+const dark = document.querySelector(".dark");
 
-localStorage.getItem("mode")
-  ? document.body.classList.add("light")
-  : document.body.classList.remove("light");
+if (localStorage.getItem("mode")) {
+  document.body.classList.add("light");
+  light.classList.remove("show");
+  dark.classList.add("show");
+} else {
+  document.body.classList.remove("light");
+  light.classList.add("show");
+  dark.classList.remove("show");
+}
 
 btnMode.addEventListener("click", () => {
   if (!document.body.classList.contains("light")) {
     document.body.classList.add("light");
     localStorage.setItem("mode", "light");
+    light.classList.remove("show");
+    dark.classList.add("show");
   } else {
     document.body.classList.remove("light");
     localStorage.setItem("mode", "");
+    light.classList.add("show");
+    dark.classList.remove("show");
   }
 });
